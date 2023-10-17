fb0f732d7bcc1fe536a9d830e018dbaf49e5539c
Thomas Ciro <thomyciro@gmail.com>
2023-10-16 19:28:06 -0500

    Creacion del proyecto

---
+++ Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_3_Andres_Flórez_y_Thomas_Ciro
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
