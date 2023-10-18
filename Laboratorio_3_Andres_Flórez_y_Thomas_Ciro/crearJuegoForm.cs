5131bc24d32d5600daf48cc82b0dfff9120f4e26
Thomas Ciro <thomyciro@gmail.com>
2023-10-18 15:54:07 -0500

    formulario persona

---
+++ Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/crearJuegoForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_3_Andres_Flórez_y_Thomas_Ciro
+{
+    public partial class crearJuegoForm : Form
+    {
+        public crearJuegoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
--- Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/datosJugador.Designer.cs
+++ Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/datosJugador.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.label11 = new System.Windows.Forms.Label();
-            this.nombreText = new System.Windows.Forms.TextBox();
-            this.emailText = new System.Windows.Forms.TextBox();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
-            this.EndulzIdealText = new System.Windows.Forms.TextBox();
-            this.RegaloIdealText = new System.Windows.Forms.TextBox();
+            this.EndulzIdealTextBox = new System.Windows.Forms.TextBox();
+            this.RegaloIdealTextBox = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
             this.label10 = new System.Windows.Forms.Label();
@@ -52,32 +52,32 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(3, 23);
+            this.label11.Location = new System.Drawing.Point(3, 37);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(111, 23);
+            this.label11.Size = new System.Drawing.Size(235, 37);
             this.label11.TabIndex = 1;
             this.label11.Text = "Nombre:";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // nombreText
+            // nombreTextBox
             // 
-            this.nombreText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.nombreTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.nombreText.Location = new System.Drawing.Point(120, 26);
-            this.nombreText.Name = "nombreText";
-            this.nombreText.Size = new System.Drawing.Size(213, 22);
-            this.nombreText.TabIndex = 6;
+            this.nombreTextBox.Location = new System.Drawing.Point(244, 40);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(444, 22);
+            this.nombreTextBox.TabIndex = 6;
             // 
-            // emailText
+            // emailTextBox
             // 
-            this.emailText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.emailTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.emailText.Location = new System.Drawing.Point(120, 49);
-            this.emailText.Name = "emailText";
-            this.emailText.Size = new System.Drawing.Size(213, 22);
-            this.emailText.TabIndex = 7;
+            this.emailTextBox.Location = new System.Drawing.Point(244, 77);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(444, 22);
+            this.emailTextBox.TabIndex = 7;
             // 
             // label9
             // 
@@ -87,7 +87,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(3, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(336, 31);
+            this.label9.Size = new System.Drawing.Size(691, 41);
             this.label9.TabIndex = 0;
             this.label9.Text = "Añadir Jugador";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -99,9 +99,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(3, 46);
+            this.label12.Location = new System.Drawing.Point(3, 74);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(111, 23);
+            this.label12.Size = new System.Drawing.Size(235, 37);
             this.label12.TabIndex = 2;
             this.label12.Text = "e-mail:";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -113,9 +113,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label13.AutoSize = true;
             this.tableLayoutPanel6.SetColumnSpan(this.label13, 2);
-            this.label13.Location = new System.Drawing.Point(3, 69);
+            this.label13.Location = new System.Drawing.Point(3, 111);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(330, 23);
+            this.label13.Size = new System.Drawing.Size(685, 37);
             this.label13.TabIndex = 3;
             this.label13.Text = "Preferencias";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,9 +127,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(3, 92);
+            this.label14.Location = new System.Drawing.Point(3, 148);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(111, 40);
+            this.label14.Size = new System.Drawing.Size(235, 40);
             this.label14.TabIndex = 4;
             this.label14.Text = "Endulzada ideal:";
             this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -140,34 +140,34 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(3, 132);
+            this.label15.Location = new System.Drawing.Point(3, 188);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(111, 42);
+            this.label15.Size = new System.Drawing.Size(235, 41);
             this.label15.TabIndex = 5;
             this.label15.Text = "Regalo Ideal:";
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // EndulzIdealText
+            // EndulzIdealTextBox
             // 
-            this.EndulzIdealText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.EndulzIdealTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.EndulzIdealText.Location = new System.Drawing.Point(120, 95);
-            this.EndulzIdealText.Multiline = true;
-            this.EndulzIdealText.Name = "EndulzIdealText";
-            this.EndulzIdealText.Size = new System.Drawing.Size(213, 34);
-            this.EndulzIdealText.TabIndex = 8;
+            this.EndulzIdealTextBox.Location = new System.Drawing.Point(244, 151);
+            this.EndulzIdealTextBox.Multiline = true;
+            this.EndulzIdealTextBox.Name = "EndulzIdealTextBox";
+            this.EndulzIdealTextBox.Size = new System.Drawing.Size(444, 34);
+            this.EndulzIdealTextBox.TabIndex = 8;
             // 
-            // RegaloIdealText
+            // RegaloIdealTextBox
             // 
-            this.RegaloIdealText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.RegaloIdealTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.RegaloIdealText.Location = new System.Drawing.Point(120, 135);
-            this.RegaloIdealText.Multiline = true;
-            this.RegaloIdealText.Name = "RegaloIdealText";
-            this.RegaloIdealText.Size = new System.Drawing.Size(213, 36);
-            this.RegaloIdealText.TabIndex = 9;
+            this.RegaloIdealTextBox.Location = new System.Drawing.Point(244, 191);
+            this.RegaloIdealTextBox.Multiline = true;
+            this.RegaloIdealTextBox.Name = "RegaloIdealTextBox";
+            this.RegaloIdealTextBox.Size = new System.Drawing.Size(444, 35);
+            this.RegaloIdealTextBox.TabIndex = 9;
             // 
             // tableLayoutPanel5
             // 
@@ -176,16 +176,16 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel5.ColumnCount = 1;
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel5.Controls.Add(this.label9, 0, 0);
             this.tableLayoutPanel5.Controls.Add(this.tableLayoutPanel6, 0, 1);
             this.tableLayoutPanel5.Controls.Add(this.addJugador, 0, 2);
-            this.tableLayoutPanel5.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel5.Controls.Add(this.label9, 0, 0);
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(43, 24);
             this.tableLayoutPanel5.Name = "tableLayoutPanel5";
             this.tableLayoutPanel5.RowCount = 3;
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel5.Size = new System.Drawing.Size(342, 242);
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(697, 307);
             this.tableLayoutPanel5.TabIndex = 2;
             // 
             // tableLayoutPanel6
@@ -202,11 +202,11 @@
             this.tableLayoutPanel6.Controls.Add(this.label13, 0, 3);
             this.tableLayoutPanel6.Controls.Add(this.label14, 0, 4);
             this.tableLayoutPanel6.Controls.Add(this.label15, 0, 5);
-            this.tableLayoutPanel6.Controls.Add(this.nombreText, 1, 1);
-            this.tableLayoutPanel6.Controls.Add(this.emailText, 1, 2);
-            this.tableLayoutPanel6.Controls.Add(this.EndulzIdealText, 1, 4);
-            this.tableLayoutPanel6.Controls.Add(this.RegaloIdealText, 1, 5);
-            this.tableLayoutPanel6.Location = new System.Drawing.Point(3, 34);
+            this.tableLayoutPanel6.Controls.Add(this.nombreTextBox, 1, 1);
+            this.tableLayoutPanel6.Controls.Add(this.emailTextBox, 1, 2);
+            this.tableLayoutPanel6.Controls.Add(this.EndulzIdealTextBox, 1, 4);
+            this.tableLayoutPanel6.Controls.Add(this.RegaloIdealTextBox, 1, 5);
+            this.tableLayoutPanel6.Location = new System.Drawing.Point(3, 44);
             this.tableLayoutPanel6.Name = "tableLayoutPanel6";
             this.tableLayoutPanel6.RowCount = 6;
             this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
@@ -215,7 +215,7 @@
             this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel6.Size = new System.Drawing.Size(336, 174);
+            this.tableLayoutPanel6.Size = new System.Drawing.Size(691, 229);
             this.tableLayoutPanel6.TabIndex = 1;
             // 
             // label10
@@ -227,7 +227,7 @@
             this.tableLayoutPanel6.SetColumnSpan(this.label10, 2);
             this.label10.Location = new System.Drawing.Point(3, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(330, 23);
+            this.label10.Size = new System.Drawing.Size(685, 37);
             this.label10.TabIndex = 0;
             this.label10.Text = "Datos personales";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -236,7 +236,7 @@
             // addJugador
             // 
             this.addJugador.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.addJugador.Location = new System.Drawing.Point(123, 215);
+            this.addJugador.Location = new System.Drawing.Point(300, 280);
             this.addJugador.Name = "addJugador";
             this.addJugador.Size = new System.Drawing.Size(96, 23);
             this.addJugador.TabIndex = 2;
@@ -248,7 +248,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(821, 381);
             this.Controls.Add(this.tableLayoutPanel5);
             this.Name = "datosJugador";
             this.Text = "datosJugador";
@@ -263,8 +263,8 @@
         #endregion
 
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.TextBox nombreText;
-        private System.Windows.Forms.TextBox emailText;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
@@ -272,8 +272,8 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.TextBox EndulzIdealText;
-        private System.Windows.Forms.TextBox RegaloIdealText;
+        private System.Windows.Forms.TextBox EndulzIdealTextBox;
+        private System.Windows.Forms.TextBox RegaloIdealTextBox;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
         private System.Windows.Forms.Button addJugador;
     }
