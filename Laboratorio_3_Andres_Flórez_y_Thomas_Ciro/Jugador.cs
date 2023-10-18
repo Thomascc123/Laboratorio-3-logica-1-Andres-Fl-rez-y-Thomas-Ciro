d26933b85e4b3ee95fa0436391de2524b653645a
admiin <admiin@LAPTOP-I3LSL3KM>
2023-10-17 21:22:10 -0500

    Creación interfaz

---
+++ Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/Jugador.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_Andres_Flórez_y_Thomas_Ciro
+{
+    internal class Jugador
+    {
+        String nombre;
+        String correo;
+        String endulzadaIdeal;
+        String regaloIdeal;
+        Jugador amigoSecreto;
+
+        public  Jugador()
+        {
+            nombre = "Sin asignar";
+            correo = "Sin asignar";
+            endulzadaIdeal = "Sin asignar";
+            regaloIdeal = "Sin asignar";
+        }
+
+        public Jugador(string nombre, string correo, string endulzadaIdeal,
+            string regaloIdeal)
+        {
+            this.nombre = nombre;
+            this.correo = correo;
+            this.endulzadaIdeal = endulzadaIdeal;
+            this.regaloIdeal = regaloIdeal;
+        }
+
+        public String getNombre()
+        {
+            return nombre;
+        }
+
+        public String getCorreo()
+        {
+            return correo;
+        }
+        public String getEndulzadaIdeal()
+        {
+            return endulzadaIdeal;
+        }
+
+        public String getRegaloIdeal()
+        {
+            return regaloIdeal;
+        }
+
+        public Jugador getAmigoSecreto()
+        {
+            return amigoSecreto;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setCorreo(String correo)
+        {
+            this.correo = correo;
+        }
+
+        public void setEndulzadaIdeal(String endulzadaIdeal)
+        {
+            this.endulzadaIdeal = endulzadaIdeal;
+        }
+
+        public void setRegaloIdeal(String regaloIdeal)
+        {
+            this.regaloIdeal = regaloIdeal;
+        }
+
+        public void setAmigoSecreto(Jugador amigoSecreto)
+        {
+            this.amigoSecreto = amigoSecreto;
+        }
+
+        public String[] mostrarDatos()
+        {
+            String[] datos = new string[5];
+            datos[0] = this.nombre;
+            datos[1] = this.correo;
+            datos[2] = this.endulzadaIdeal;
+            datos[3] = this.regaloIdeal;
+            datos[4] = this.amigoSecreto.getNombre();
+            return datos;
+        }
+
+    }
+}
