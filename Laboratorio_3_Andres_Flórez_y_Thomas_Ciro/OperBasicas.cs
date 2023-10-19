01c98e097a79ea853fc0fbb6f023f24ec8af24e6
admiin <admiin@LAPTOP-I3LSL3KM>
2023-10-19 16:56:55 -0500

    funciones de archivo

--- Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/OperBasicas.cs
+++ Laboratorio_3_Andres_Flórez_y_Thomas_Ciro/OperBasicas.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Linq.Expressions;
 
 namespace Laboratorio_3_Andres_Flórez_y_Thomas_Ciro
 {
@@ -47,6 +49,115 @@ namespace Laboratorio_3_Andres_Flórez_y_Thomas_Ciro
                 asignacion[i] = subAsignacion[i - 1];
             }
             return asignacion;
+        }
+
+        public bool crearArchivo(String nombre, int id)
+        {
+            String ruta = System.IO.Directory.GetCurrentDirectory();
+            ruta = ruta + @"\" + nombre +" "+ id + ".txt";
+
+            try
+            {
+                using(FileStream fs = File.Create(ruta))
+                {
+                    byte[] texto = new UTF8Encoding(true).GetBytes("Nuevo Juego Creado"+id);
+                    // Add some information to the file.
+                    fs.Write(texto, 0, texto.Length);
+                    fs.Close();
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool escribirLinea(String nombreArchivo, String linea)
+        {
+            String ruta = System.IO.Directory.GetCurrentDirectory();
+            ruta = ruta + @"\" + nombreArchivo + ".txt";
+
+            try
+            {
+                using (FileStream fs = File.OpenWrite(ruta))
+                {
+                    byte[] texto = new UTF8Encoding(true).GetBytes(linea);
+                    // Add some information to the file.
+                    fs.Write(texto, 0, texto.Length);
+                    fs.Close();
+                }
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool sobreEscribirLinea(String nombreArchivo, String lineaNUeva, int pos)
+        {
+            String nombreCopia = nombreArchivo + "Copia";
+            String ruta = System.IO.Directory.GetCurrentDirectory();
+            ruta = ruta + @"\" + nombreArchivo + ".txt";
+            if (crearArchivo(nombreCopia, 0))
+            {
+                try
+                {
+                    using (StreamReader sr = File.OpenText(ruta))
+                    {
+                        String linea = "";
+                        int i = 0;
+                        while ((linea = sr.ReadLine()) != null)
+                        {
+                            if (i == pos)
+                            {
+                                escribirLinea(nombreArchivo, lineaNUeva);
+                            }
+                            else
+                            {
+                                escribirLinea(nombreArchivo, linea);
+                            }
+                        }
+                        sr.Close();
+                    }
+                    String rutaCopia = System.IO.Directory.GetCurrentDirectory();
+                    rutaCopia = rutaCopia + @"\" + nombreCopia + "0.txt";
+                    File.Copy(rutaCopia, ruta);
+                    File.Delete(rutaCopia);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public String leerLinea(String nombreArchivo) 
+        {
+            String ruta = System.IO.Directory.GetCurrentDirectory();
+            ruta = ruta + @"\" + nombreArchivo + ".txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(ruta))
+                {
+                    String linea = "";
+                    linea = sr.ReadLine();
+                    return linea;
+                }
+
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
         }
     }
 
